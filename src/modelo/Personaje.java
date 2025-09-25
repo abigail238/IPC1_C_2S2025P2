d8cfc7c5c3a3973c2cfdbb110a3623ed8bb39af9
abigail238 <3726005300115@ingenieria.usac.edu.gt>
2025-09-24 20:09:21 -0600

    first commit

---
+++ src/modelo/Personaje.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modelo;
+
+        
+/**
+ *
+ * @author Lorena
+ */
+public class Personaje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+        MenuPrincipal panta = new MenuPrincipal();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+    }
+
+    //SWING -> LIBRERIA GRAFICA DE JAVA
+
+    
+           
+       }
+        
+     
+    
+    
+    
+    
+    
+    
+
+
+
+
+
